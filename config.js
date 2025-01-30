0a2447c201f7a9d2d6cfefe66689cc7e52ffa2e4
Decrypt <louis.vicomte@protonmail.com>
2025-01-30 20:08:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ const config = {
     openaiApiKey: process.env.API_KEY,
     triggers: {
         quoiChance: 0.7, // 70% de chance de répondre à "quoi"
-        mockChance: 0.01, // 15% de chance de mocker
-        randomInterventionChance: 0.05, // 5% de chance d'intervenir random
-        waterReminderChance: 0.01 // 1% de chance de rappeler de boire de l'eau
+        mockChance: 0.003, // 0.3% de chance de mocker
+        randomInterventionChance: 0.003, // 0.3% de chance d'intervenir random
+        waterReminderChance: 0.003 // 0.3% de chance de rappeler de boire de l'eau
     }
 };
 
