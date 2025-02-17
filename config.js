4e3d4b03c44d2e19aa40b7611abbfcac56f46db8
Decrypt <louis.vicomte@protonmail.com>
2025-02-17 20:25:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const config = {
         quoiChance: 0.5, // 70% de chance de répondre à "quoi"
         mockChance: 0.005, // 0.5% de chance de mocker
         randomInterventionChance: 0.005, // 0.5% de chance d'intervenir random
-        waterReminderChance: 0.005, // 0.5% de chance de rappeler de boire de l'eau
-        haikuChance: 0.005 // 0.5% de chance de faire un haiku
+        waterReminderChance: 0.001, // 0.1% de chance de rappeler de boire de l'eau
+        haikuChance: 0.001 // 0.1% de chance de faire un haiku
     }
 };
 
